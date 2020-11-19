a81afcd10b0ddaec40aa792bc563ad393a31184e
ankhafizov <ankhafizov1998@yandex.ru>
2020-11-19 16:05:23 +0300

    added binarization with paganin's correction

--- phase_contrast.py
+++ phase_contrast.py
@@ -3,10 +3,19 @@ from PIL import Image
 import numpy as np
 import os
 import h5py
+from scipy.signal import fftconvolve
+from skimage.filters import threshold_otsu
 
 
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 PHANTOM_DB_FOLDER_NAME = 'database'
+
+
+def filter_mean(img):
+    kernel_size = 3
+    kernel_shape = np.ones(img.ndim, dtype=int) * kernel_size
+    kernel = np.ones(kernel_shape, dtype=int)
+    return fftconvolve(img, kernel, mode='same')
 
 
 def split_list(lst, n):
@@ -38,25 +47,60 @@ def get_img(file_name):
     return dataset
 
 
-data_folder = '/nfs/synology-tomodata/external_data/tomo/Diamond/I13'+\
-              '/2020_02/recon/123495/full_recon/20200206141126_123495/TiffSaver-tomo'
+def poganins_correction(img, k=20, mu=25e-8):
+    data_fft = np.fft.fft2(img)
 
-file_names = Path(data_folder).glob('*.tiff')
-file_names = list(file_names)
-print(len(file_names))
+    freq = np.fft.fftfreq(img.shape[0])
+    fx, fy = np.meshgrid(freq, freq)
+    f2 = np.sqrt(fx**2+fy**2)
 
-number_of_slices_in_group = 100
-file_groups = split_list(file_names, number_of_slices_in_group)
-print("total number of slice_groups: ", len(file_groups))
+    data_corr_fft = data_fft/np.sqrt((k * f2**2+mu))
+    data_corr = np.abs(np.fft.ifft2(data_corr_fft))
 
-number_of_slice = 0
-img3d = []
-for file_name in file_groups[number_of_slice]:
-    img2d = np.array(Image.open(file_name))
-    img3d.append(img2d)
-img3d=np.asarray(img3d)
+    return data_corr
 
-print(img3d.shape)
 
-save(img3d, 'test file.h5')
-print(get_img('test file.h5').shape)
+def binarize_slice(img2d):
+    img2d_corrected = poganins_correction(img2d, k=20, mu=25e-8)
+
+    #probably look better
+    img2d_corrected_filtered = filter_mean(img2d_corrected)
+    thresh = threshold_otsu(img2d_corrected_filtered)
+
+    return img2d_corrected_filtered > thresh
+
+
+def binarize_volume(volume):
+    volume_bin = []
+
+    total_num_of_2d_slices = len(volume)
+    for i, img2d in enumerate(volume):
+        volume_bin.append(binarize_slice(img2d))
+        print(f"{i+1} out of {total_num_of_2d_slices}")
+
+    return volume_bin
+
+
+if __name__='main':
+    data_folder = '/nfs/synology-tomodata/external_data/tomo/Diamond/I13'+\
+                '/2020_02/recon/123495/full_recon/20200206141126_123495/TiffSaver-tomo'
+
+    file_names = Path(data_folder).glob('*.tiff')
+    file_names = list(file_names)
+    print(len(file_names))
+
+    number_of_slices_in_group = 100
+    file_groups = split_list(file_names, number_of_slices_in_group)
+    print("total number of slice_groups: ", len(file_groups))
+
+    number_of_slice = 0
+    img3d = []
+    for file_name in file_groups[number_of_slice]:
+        img2d = np.array(Image.open(file_name))
+        img3d.append(img2d)
+    img3d=np.asarray(img3d)
+
+    img3d_binarized = binarize_volume(img3d)
+    save(img3d_binarized, 'test_file.h5')
+
+    print(get_img('test_file.h5').shape)
