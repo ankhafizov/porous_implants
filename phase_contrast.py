4477d101c90e4fa64fd54a6336fe733b8b466824
ankhafizov <ankhafizov1998@yandex.ru>
2020-11-19 15:01:07 +0300

    added save-read system for img

--- phase_contrast.py
+++ phase_contrast.py
@@ -1,14 +1,41 @@
 from pathlib import Path
 from PIL import Image
 import numpy as np
+import os
+import h5py
+
+
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+PHANTOM_DB_FOLDER_NAME = 'database'
 
 
 def split_list(lst, n):
     arr = []
     for i in range(0, len(lst), n):
-        print(i, i+n)
         arr.append(lst[i:i + n])
     return arr
+
+
+def save(img, file_name):
+    db_folder = os.path.join(SCRIPT_PATH, PHANTOM_DB_FOLDER_NAME)
+    if not os.path.isdir(db_folder):
+        os.mkdir(db_folder)
+
+    db_path = os.path.join(db_folder, file_name)
+    if os.path.isfile(db_path):
+        os.remove(db_path)
+    
+    with h5py.File(db_path, 'a') as hdf:
+        hdf.create_dataset(name = file_name, data = img, compression='gzip', compression_opts=0)
+
+
+def get_img(file_name):
+    file_path = os.path.join(SCRIPT_PATH, PHANTOM_DB_FOLDER_NAME, file_name)
+
+    with h5py.File(file_path, 'r') as hdf:
+        dataset = hdf.get(name = file_name)
+        dataset = dataset[()]
+    return dataset
 
 
 data_folder = '/nfs/synology-tomodata/external_data/tomo/Diamond/I13'+\
@@ -30,3 +57,6 @@ for file_name in file_groups[number_of_slice]:
 img3d=np.asarray(img3d)
 
 print(img3d.shape)
+
+save(img3d, 'test file.h5')
+print(get_img('test file.h5').shape)
