b5074f0b9ee32560fae240f3818e8290ac8fa31b
ankhafizov <ankhafizov1998@yandex.ru>
2020-11-27 18:27:57 +0300

    added path file

---
+++ file_paths.py
@@ -0,0 +1,24 @@
+def get_path(file_id):
+    if  file_id == '123493':
+        data_folder = '/nfs/synology-tomodata/external_data/tomo/Diamond/I13'+\
+                    f'/2020_02/recon/123493/full_recon/20200206101055_123493/TiffSaver-tomo'
+    elif  file_id == '123494':
+        data_folder = '/nfs/synology-tomodata/external_data/tomo/Diamond/I13'+\
+                    f'/2020_02/recon/123494/full_recon/20200206140043_123494/TiffSaver-tomo'
+    elif  file_id == '123495':
+        data_folder = '/nfs/synology-tomodata/external_data/tomo/Diamond/I13'+\
+                    f'/2020_02/recon/123495/full_recon/20200206141126_123495/TiffSaver-tomo'
+    elif  file_id == '123496':
+        data_folder = '/nfs/synology-tomodata/external_data/tomo/Diamond/I13'+\
+                    f'/2020_02/recon/123496/full_recon/20200206141825_123496/TiffSaver-tomo'
+    elif  file_id == '123497':
+        data_folder = '/nfs/synology-tomodata/external_data/tomo/Diamond/I13'+\
+                    f'/2020_02/recon/123497/full_recon/20200206143333_123497/TiffSaver-tomo'
+    elif  file_id == '123498':
+        data_folder = '/nfs/synology-tomodata/external_data/tomo/Diamond/I13'+\
+                    f'/2020_02/recon/123498/full_recon/20200206145231_123498/TiffSaver-tomo'
+    elif  file_id == '123499':
+        data_folder = '/nfs/synology-tomodata/external_data/tomo/Diamond/I13'+\
+                    f'/2020_02/recon/123499/full_recon/20200206150204_123499/TiffSaver-tomo'
+
+    return data_folder
