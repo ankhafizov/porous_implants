230ed3c31b98ec4816c3b4f395c9d4aff7bb3e0c
ankhafizov <ankhafizov1998@yandex.ru>
2020-12-09 12:48:08 +0300

    trial to got invariant in fan beam by log((R1+R2)/R1) failed

--- fan_beam.py
+++ fan_beam.py
@@ -11,8 +11,15 @@ blobns = 2
 porsty = 0.5
 shape = 100
 phantom = generator.blobs([shape, shape], porosity=1 - porsty, blobiness=blobns)
-sinogram_fan = np.transpose(ae.astra_fp_2d_fan(phantom, angles, 1000, 20))
+
+source_object = 200
+object_det = 200
 sinogram_parallel = radon(phantom, theta=angles, circle=False)
+sinogram_fan = np.transpose(ae.astra_fp_2d_fan(phantom,
+                                               angles,
+                                               source_object,
+                                               object_det,
+                                               detector_size=300))
 
 fig, ax = plt.subplots(1,2)
 ax[0].imshow(sinogram_fan, cmap='gray')
@@ -21,11 +28,20 @@ ax[1].imshow(sinogram_parallel, cmap='gray')
 ax[1].set_title('sinogram_parallel')
 dm.save_plot(fig, 'plots', 'sinograms')
 
-column_nums=[10, 20, 40, 50, 130]
+print('--------------sinogram_fan-----------------')
+column_nums=[np.random.randint(0, sinogram_parallel.shape[0]) for _ in range(10)]
 for column_num in column_nums:
     print(f'integral of {column_num} of sinogram_fan:', np.sum(sinogram_fan[:, column_num]))
+print(f'std: {np.std([np.sum(sinogram_fan[:, column_num]) for column_num in column_nums])}')
 
-print('-------------------------------')
+print('--------------sinogram_fan + ln[(R1+R2)/R1]-----------------')
+sinogram_fan = sinogram_fan + np.log((source_object + object_det) / source_object)
+for column_num in column_nums:
+    print(f'integral of {column_num} of sinogram_fan:', np.sum(sinogram_fan[:, column_num]))
+print(f'std: {np.std([np.sum(sinogram_fan[:, column_num]) for column_num in column_nums])}')
+
+print('--------------sinogram_parallel-----------------')
 
 for column_num in column_nums:
     print(f'integral of {column_num} of sinogram_parallel:', np.sum(sinogram_parallel[:, column_num]))
+print(f'std: {np.std([np.sum(sinogram_parallel[:, column_num]) for column_num in column_nums])}')
