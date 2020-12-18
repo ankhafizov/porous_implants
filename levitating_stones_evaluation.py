dba7a66597fb4476bfe3a8c6d8ba7219c7ed29d4
ankhafizov <ankhafizov1998@yandex.ru>
2020-12-18 17:31:28 +0300

    feture: diameter calculations

--- levitating_stones_evaluation.py
+++ levitating_stones_evaluation.py
@@ -40,7 +40,7 @@ def get_structure(neighbors_num=6):
 
 def get_closed_pores(bin_volume, structure_neighbors_num=6):
     """
-    function returns a bin_volume of levitating pores for the
+    function returns a bin_volume of closed pores for the
     chosen neighbor voxels configuration
 
     """
@@ -54,12 +54,17 @@ def get_closed_pores(bin_volume, structure_neighbors_num=6):
     return levitating_volume
 
 
-def plot_pore_volume_histogram(bin_volume,
+def plot_pore_size_histogram(bin_volume,
                                structure_neighbors_num=6,
+                               size_type="volume",
                                num_of_bins=35,
                                max_x_value=None,
                                save_plot=False):
-
+    """
+    function returns a histogram of closed pores' sizes for the
+    chosen neighbor voxels configuration.
+    Note: size_type must be either "volume" or "diameter"
+    """
     figure, ax = plt.subplots(figsize=(10, 10))
 
     lv = get_closed_pores(bin_volume, structure_neighbors_num)
@@ -70,33 +75,37 @@ def plot_pore_volume_histogram(bin_volume,
         ax.set_title('No pores detected', color="red")
         return None
 
-    pore_volume_distribution = get_pore_volume_distribution(lv,
-                                                            structure_neighbors_num)
+    pore_size_distribution = get_pore_volume_distribution(lv,
+                                                          structure_neighbors_num)
+    if size_type == "volume":
+        continue
+    elif size_type == "diameter":
+        pore_size_distribution = recount_volumes_to_diameters(pore_size_distribution)
 
 
     if not max_x_value:
-        max_x_value = np.max(pore_volume_distribution)
+        max_x_value = np.max(pore_size_distribution)
 
     bins = np.linspace(0, max_x_value, num_of_bins+1)
 
     stats = (f'total_num_of_pores = {total_num_of_pores}\n'
-             f'MAX pore volume = {np.max(pore_volume_distribution):.0f}\n'
-             f'MIN pore volume = {np.min(pore_volume_distribution):.0f}')
+             f'MAX pore {size_type} = {np.max(pore_size_distribution):.0f}\n'
+             f'MIN pore {size_type} = {np.min(pore_size_distribution):.0f}')
     bbox = dict(boxstyle='round', fc='blanchedalmond', ec='orange', alpha=0.35)
     ax.text(0.95, 0.3, stats, fontsize=9, bbox=bbox,
             transform=ax.transAxes, horizontalalignment='right')
 
-    ax.hist(pore_volume_distribution,
+    ax.hist(pore_size_distribution,
             bins=bins,
             log=True,
             edgecolor='k')
 
-    ax.set_title(f'pores_volume_distribution | connectivity number={structure_neighbors_num}')
-    ax.set_xlabel('volume in voxels')
+    ax.set_title(f'pores_{size_type}_distribution | connectivity number={structure_neighbors_num}')
+    ax.set_xlabel(f'{size_type} in voxels')
     ax.grid()
 
     if save_plot:
-        save(figure, "plots", "pores_volume_distribution")
+        save(figure, "plots", f"pores_{size_type}_distribution")
 
 
 def get_pore_volume_distribution(levitatting_volume, structure_neighbors_num):
@@ -105,3 +114,9 @@ def get_pore_volume_distribution(levitatting_volume, structure_neighbors_num):
     pore_volume_distribution = np.unique(connected_components, return_counts=True)[1][1:]
 
     return pore_volume_distribution
+
+
+def pore_size_distribution(volumes):
+    volumes = np.asarray(volumes)
+    diameters = 2 * np.power((volumes*3)/(4*np.pi), 1/3)
+    return diameters
