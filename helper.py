cae563b5301663a08f9774573903798767114097
ankhafizov <ankhafizov1998@yandex.ru>
2021-01-26 22:18:59 +0300

    added paste function

--- helper.py
+++ helper.py
@@ -25,6 +25,13 @@ def crop(img, shape, center=None):
     return img[tuple(ranges)]
 
 
+def paste(matrix, fragmet, center):
+    h, w = np.asarray(fragmet.shape)
+    y0, x0 = np.asarray(center) - np.asarray(fragmet.shape) // 2
+    matrix[y0:y0+h, x0:x0+w][:] = fragmet
+    return matrix
+
+
 def write_item_to_file(value, filename, db_folder):
     if not os.path.isdir(db_folder):
         os.mkdir(db_folder)
