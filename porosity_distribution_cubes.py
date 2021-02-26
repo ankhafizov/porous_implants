6508ff76f925ce9639b5adcaf41d2495aa1d48d7
ankhafizov <ankhafizov1998@yandex.ru>
2021-02-26 17:19:42 +0300

    added 3d processing

--- porosity_distribution_cubes.py
+++ porosity_distribution_cubes.py
@@ -4,26 +4,36 @@ import numpy as np
 
 import data_manager as dm
 from helper import crop
+from helper import crop, paste, get_2d_slice_of_sample_from_database
+from phase_contrast_restoration import get_img as get_bin_img
 
 plt.style.use('seaborn-whitegrid')
 plt.rcParams.update({'font.size':22})
 
-PIXEL_SIZE_MM = 1
+PIXEL_SIZE_MM = 0.8125 * 10**-3
 
 def divide_image_into_cubic_fragments(img, edge_size):
     count_of_center_points = np.min(img.shape) // edge_size
     img_fragments = []
 
-    for x_coord in np.arange(count_of_center_points) + 0.5:
-        for y_coord in np.arange(count_of_center_points) + 0.5:
-            center_coords = np.ceil(np.asarray([x_coord, y_coord]) * edge_size).astype(int)
-            img_fragment = crop(img, (edge_size, edge_size), center_coords)
-            img_fragments.append(img_fragment)
-    
+    if img.ndim == 2:
+        for x_coord in np.arange(count_of_center_points) + 0.5:
+            for y_coord in np.arange(count_of_center_points) + 0.5:
+                center_coords = np.ceil(np.asarray([x_coord, y_coord]) * edge_size).astype(int)
+                img_fragment = crop(img, (edge_size, edge_size), center_coords)
+                img_fragments.append(img_fragment)
+    elif img.ndim == 3:
+        for x_coord in np.arange(count_of_center_points) + 0.5:
+            for y_coord in np.arange(count_of_center_points) + 0.5:
+                for z_coord in np.arange(count_of_center_points) + 0.5:
+                    center_coords = np.ceil(np.asarray([x_coord, y_coord, z_coord]) * edge_size).astype(int)
+                    img_fragment = crop(img, (edge_size, edge_size, edge_size), center_coords)
+                    img_fragments.append(img_fragment)
+
     return img_fragments
 
 
-def get_porosity_histogram_disrtibution(img_fragments, title="sample #"):
+def get_porosity_histogram_disrtibution(img_fragments, file_id):
     get_porosity = lambda bin_img: np.sum(bin_img) / bin_img.size
 
     porosities = [get_porosity(img_fragment) for img_fragment in img_fragments]
@@ -40,14 +50,20 @@ def get_porosity_histogram_disrtibution(img_fragments, title="sample #"):
     ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=20,
             verticalalignment='top', bbox=props)
 
-    ax.set_title(title)
+    ax.set_title(f"sample id {file_id}")
 
     return fig
 
 
-phantom = blobs([2100,2100], 0.5, 3)
+if __name__=='__main__':
+    file_id='123497'
+    bin_img = ~get_bin_img(file_id+'.h5').astype(bool)
 
-img_fragments = divide_image_into_cubic_fragments(phantom, edge_size=400)
-print(len(img_fragments))
+    img_fragments = divide_image_into_cubic_fragments(bin_img, edge_size=400)
+    print(len(img_fragments))
 
-dm.save_plot(get_porosity_histogram_disrtibution(img_fragments), 'phantom section', 'phantom_hist')
+    fig, ax = plt.subplots(figsize=(10,10))
+    ax.imshow(bin_img[0])
+    dm.save_plot(fig, 'phantom section', 'section')
+
+    dm.save_plot(get_porosity_histogram_disrtibution(img_fragments, file_id=file_id), 'phantom section', 'hist')
