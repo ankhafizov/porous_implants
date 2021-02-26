3c48c7865732ccad63a5b78a80a9a972ea4526a8
ankhafizov <ankhafizov1998@yandex.ru>
2021-02-26 17:50:07 +0300

    added gridplot

--- porosity_distribution_cubes.py
+++ porosity_distribution_cubes.py
@@ -17,15 +17,15 @@ def divide_image_into_cubic_fragments(img, edge_size):
     img_fragments = []
 
     if img.ndim == 2:
-        for x_coord in np.arange(count_of_center_points) + 0.5:
-            for y_coord in np.arange(count_of_center_points) + 0.5:
+        for x_coord in np.arange(count_of_center_points+1) + 0.5:
+            for y_coord in np.arange(count_of_center_points+1) + 0.5:
                 center_coords = np.ceil(np.asarray([x_coord, y_coord]) * edge_size).astype(int)
                 img_fragment = crop(img, (edge_size, edge_size), center_coords)
                 img_fragments.append(img_fragment)
     elif img.ndim == 3:
-        for x_coord in np.arange(count_of_center_points) + 0.5:
-            for y_coord in np.arange(count_of_center_points) + 0.5:
-                for z_coord in np.arange(count_of_center_points) + 0.5:
+        for x_coord in np.arange(count_of_center_points+1) + 0.5:
+            for y_coord in np.arange(count_of_center_points+1) + 0.5:
+                for z_coord in np.arange(count_of_center_points+1) + 0.5:
                     center_coords = np.ceil(np.asarray([x_coord, y_coord, z_coord]) * edge_size).astype(int)
                     img_fragment = crop(img, (edge_size, edge_size, edge_size), center_coords)
                     img_fragments.append(img_fragment)
@@ -33,7 +33,18 @@ def divide_image_into_cubic_fragments(img, edge_size):
     return img_fragments
 
 
-def get_porosity_histogram_disrtibution(img_fragments, file_id):
+def plot_grid(img, ax, step):
+    count_of_center_points = np.min(img.shape) // edge_size
+
+    for x_edge_coord in np.arange(count_of_center_points+1)*edge_size:
+        for y_edge_coord in np.arange(count_of_center_points+1)*edge_size:
+            ax.axhline(y_edge_coord, color='red', linewidth=2)
+            ax.axvline(x_edge_coord, color='red', linewidth=2)
+
+    return ax
+
+
+def get_porosity_histogram_disrtibution(img_fragments, file_id, sample_shape):
     get_porosity = lambda bin_img: np.sum(bin_img) / bin_img.size
 
     porosities = [get_porosity(img_fragment) for img_fragment in img_fragments]
@@ -43,8 +54,12 @@ def get_porosity_histogram_disrtibution(img_fragments, file_id):
     ax.set_ylabel("count")
     ax.set_xlim([0,1])
 
-    textstr = (f'$\sigma={np.std(porosities):.2f}$ \n $\mu={np.mean(porosities):.2f}$') + \
-              (f'\n сторона кубика: {img_fragments[0].shape[0]*PIXEL_SIZE_MM} мм')
+    textstr = (f'$\sigma={np.std(porosities):.2f}$;') + \
+              (f'\n $\mu={np.mean(porosities):.2f}$;') + \
+              (f'\n Размеры образца: \n {sample_shape[0]*PIXEL_SIZE_MM:.2f}x') + \
+              (f'{sample_shape[1]*PIXEL_SIZE_MM:.2f}x{sample_shape[2]*PIXEL_SIZE_MM:.2f} мм;') + \
+              (f'\n Сторона кубика: {img_fragments[0].shape[0]*PIXEL_SIZE_MM} мм;') + \
+              (f'\n Кубиков: {len(img_fragments)} шт.')
 
     props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
     ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=20,
@@ -57,13 +72,16 @@ def get_porosity_histogram_disrtibution(img_fragments, file_id):
 
 if __name__=='__main__':
     file_id='123497'
+    edge_size = 400
     bin_img = ~get_bin_img(file_id+'.h5').astype(bool)
 
-    img_fragments = divide_image_into_cubic_fragments(bin_img, edge_size=400)
+    img_fragments = divide_image_into_cubic_fragments(bin_img, edge_size=edge_size)
     print(len(img_fragments))
 
-    fig, ax = plt.subplots(figsize=(10,10))
-    ax.imshow(bin_img[0])
+    with plt.style.context('classic'):
+        fig, ax = plt.subplots(figsize=(10,10))
+        ax = plot_grid(bin_img[0], ax, edge_size)
+        ax.imshow(bin_img[0], cmap='gray')
     dm.save_plot(fig, 'phantom section', 'section')
 
-    dm.save_plot(get_porosity_histogram_disrtibution(img_fragments, file_id=file_id), 'phantom section', 'hist')
+    dm.save_plot(get_porosity_histogram_disrtibution(img_fragments, file_id, bin_img.shape), 'phantom section', 'hist')
