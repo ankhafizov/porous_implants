12c672481a0f95ff0d3f5e6168abae481251bd4d
ankhafizov <ankhafizov1998@yandex.ru>
2021-02-26 18:44:42 +0300

    fix: grid in rectangular image

--- porosity_distribution_cubes.py
+++ porosity_distribution_cubes.py
@@ -11,22 +11,22 @@ plt.style.use('seaborn-whitegrid')
 plt.rcParams.update({'font.size':22})
 
 PIXEL_SIZE_MM = 0.8125 * 10**-3
-SAVE_IMG_FOLDER = 'phantom section'
+SAVE_IMG_FOLDER = 'cubics synchrotron'
 
 def divide_image_into_cubic_fragments(img, edge_size):
-    count_of_center_points = np.min(img.shape) // edge_size
+    count_of_center_points = np.asarray(img.shape) // edge_size
     img_fragments = []
 
     if img.ndim == 2:
-        for x_coord in np.arange(count_of_center_points+1) + 0.5:
-            for y_coord in np.arange(count_of_center_points+1) + 0.5:
+        for x_coord in np.arange(count_of_center_points[0]+1) + 0.5:
+            for y_coord in np.arange(count_of_center_points[1]+1) + 0.5:
                 center_coords = np.ceil(np.asarray([x_coord, y_coord]) * edge_size).astype(int)
                 img_fragment = crop(img, (edge_size, edge_size), center_coords)
                 img_fragments.append(img_fragment)
     elif img.ndim == 3:
-        for x_coord in np.arange(count_of_center_points+1) + 0.5:
-            for y_coord in np.arange(count_of_center_points+1) + 0.5:
-                for z_coord in np.arange(count_of_center_points+1) + 0.5:
+        for x_coord in np.arange(count_of_center_points[0]+1) + 0.5:
+            for y_coord in np.arange(count_of_center_points[1]+1) + 0.5:
+                for z_coord in np.arange(count_of_center_points[2]+1) + 0.5:
                     center_coords = np.ceil(np.asarray([x_coord, y_coord, z_coord]) * edge_size).astype(int)
                     img_fragment = crop(img, (edge_size, edge_size, edge_size), center_coords)
                     img_fragments.append(img_fragment)
@@ -61,12 +61,13 @@ def get_porosity_histogram_disrtibution(img_fragments, file_id, sample_shape, pi
 
 
 def plot_grid(img, ax, step):
-    count_of_center_points = np.min(img.shape) // edge_size
+    count_of_center_points = np.asarray(img.shape) // edge_size
+    print(count_of_center_points[0], count_of_center_points[1])
 
-    for x_edge_coord in np.arange(count_of_center_points+1)*edge_size:
-        for y_edge_coord in np.arange(count_of_center_points+1)*edge_size:
-            ax.axhline(y_edge_coord, color='red', linewidth=2)
-            ax.axvline(x_edge_coord, color='red', linewidth=2)
+    for x_edge_coord in np.arange(count_of_center_points[0]+1)*edge_size:
+        for y_edge_coord in np.arange(count_of_center_points[1]+1)*edge_size:
+            ax.axhline(x_edge_coord, color='red', linewidth=2)
+            ax.axvline(y_edge_coord, color='red', linewidth=2)
 
     return ax
 
@@ -74,16 +75,17 @@ def plot_grid(img, ax, step):
 def save_first_section_of_img(img_3d, file_id):
     with plt.style.context('classic'):
         fig, ax = plt.subplots(figsize=(10,10))
-        ax = plot_grid(img_3d[0], ax, edge_size)
         ax.imshow(img_3d[0], cmap='gray')
+        ax = plot_grid(img_3d[0], ax, edge_size)
         ax.set_title(f'sample id {file_id}')
     dm.save_plot(fig, SAVE_IMG_FOLDER, f'section {file_id}')
 
 
 if __name__=='__main__':
-    file_id='123497'
-    edge_size = 400
+    file_id='123494'
+    edge_size = 500
     bin_img = get_bin_img(file_id+'.h5').astype(bool)
+    bin_img = bin_img[:, 400:1800, :] 
 
     img_fragments = divide_image_into_cubic_fragments(bin_img, edge_size=edge_size)
     save_first_section_of_img(bin_img, file_id)
