b1edbf4b6ef8b6aa9f6f03661f140dd13f83237a
ankhafizov <ankhafizov1998@yandex.ru>
2021-03-03 07:18:17 +0300

    refactoring: binarization func separated

---
+++ binarize_absorption_tomography.py
@@ -0,0 +1,155 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import h5py
+from skimage.filters import threshold_multiotsu
+from scipy.ndimage import gaussian_filter
+from skimage.draw import disk
+
+import file_paths
+import data_manager as dm
+import leviating_volume as lv
+from phase_contrast_restoration import save as save_to_h5
+
+
+plt.style.use('seaborn-whitegrid')
+plt.rcParams.update({'font.size':22})
+SAVE_IMG_DESKTOP_SETUP_FOLDER = 'setup bin section'
+
+# Вручную подобраны параметры для бинаризации polimer_attenuations_PDLG5002 и polimer_attenuations_PDL05
+
+polimer_attenuations_PDLG5002 = ["low",
+                                 "low",
+                                 "high",
+                                 "high",
+                                 "low",
+                                 "low", #manual 5
+                                 "high", #manual 6
+                                 "high", #manual 7
+                                 "high",
+                                 "high",
+                                 "low",
+                                 "high",
+                                 "high",
+                                 "high",
+                                 "high"]
+
+polimer_attenuations_PDL05 = ["low",
+                              "low",
+                              "low",
+                              "low",
+                              "low",
+                              "low", #manual 5
+                              "low", #manual 6
+                              "high", #manual 7
+                              "high", # 8
+                              "high", # 9
+                              "low", # 10
+                              "low", # 11
+                              "low", # 12
+                              "low", # 13
+                              "high"]
+
+
+def binarize_without_eppendorf(img3d, polimer_attenuation):
+    """
+    polimer_attenuation = "low", "high"
+    """
+    img3d_bin = []
+    for img2d in img3d:
+        thresholds = threshold_multiotsu(img2d)
+        if polimer_attenuation=="low":
+            #img2d_bin = np.logical_and(img2d < thresholds[1], img2d > thresholds[0])
+            # лучше вместе с эппендорфом
+            img2d_bin = img2d > thresholds[0]
+        elif polimer_attenuation=="high":
+            img2d_bin = img2d > thresholds[1]
+        
+        img3d_bin.append(img2d_bin)
+        
+    return np.asarray(img3d_bin)
+
+
+def plot_3_sections_multiotsu(img3d,
+                              polimer_attenuation,
+                              filename,
+                              radius_coef=0.9,
+                              folder=SAVE_IMG_DESKTOP_SETUP_FOLDER+"multy"):
+
+    fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(21, 21))
+    axes_plot, axes_hist, axes_bin = axes
+
+    section_indexes = [0, len(img3d)//2, -1]
+    img3d = gaussian_filter(img3d, sigma=3)
+
+    for ax_plot, ax_hist, i in zip(axes_plot, axes_hist, section_indexes):
+        ax_plot.imshow(img3d[i], cmap="gray")
+
+        ax_hist.hist(img3d[i].flatten(), bins=255, color="gray")
+        thresholds = threshold_multiotsu(img3d[i])
+        for thresh in thresholds:
+            ax_hist.axvline(thresh, color='red')
+
+    img3d = binarize_without_eppendorf(img3d, polimer_attenuation=polimer_attenuation)
+    img3d = lv.remove_levitating_stones(img3d)
+
+    for ax_bin, i in zip(axes_bin, section_indexes):
+        ax_bin.imshow(img3d[i], cmap="gray", interpolation=None)
+    
+        center = np.asarray(img3d[i].shape) // 2
+
+        rr, cc = disk(center, int(np.min(center)*radius_coef), shape=img3d[i].shape)
+        mask = np.zeros(img3d[i].shape, dtype=int)
+        mask[rr, cc] = True
+        mask = np.ma.masked_where(mask>0, mask)
+        ax_bin.imshow(mask, cmap="hsv", alpha=0.3)
+
+    dm.save_plot(fig, folder, 'section '+filename)
+
+
+if __name__=='__main__':
+    # выбрать тип полимера
+    polimer_type = ["PDL-05", "PDLG-5002"][1]
+
+    radius_coefs = {"PDL-05": 0.9, "PDLG-5002": 0.95}
+    polimer_attenuation = {"PDL-05": polimer_attenuations_PDL05,
+                           "PDLG-5002": polimer_attenuations_PDLG5002}
+    
+    paths = file_paths.get_benchtop_setup_paths(polimer_type)
+
+    for sample_id in range(len(paths)):
+        print(sample_id)
+        sample_name = list(paths.keys())[sample_id]
+        sample = h5py.File(paths[sample_name],'r')
+        
+        img3d = sample['Reconstruction'][:]
+        
+        fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(21, 14))
+        for ax, i in zip(axes[0], [0, len(img3d) // 2, -1]):
+            ax.imshow(img3d[i], cmap="gray")
+
+        # preview sections
+        # plot_3_sections_multiotsu(img3d,
+        #                           polimer_attenuations_PDL05[sample_id],
+        #                           str(sample_id) + ' ' + sample_name)
+                                  
+        img3d = gaussian_filter(img3d, sigma=3)
+        img3d = binarize_without_eppendorf(img3d, polimer_attenuation=polimer_attenuation[polimer_type][sample_id])
+        img3d = lv.remove_levitating_stones(img3d)
+        
+        section_shape = img3d[0].shape
+        center = np.asarray(section_shape) // 2
+        rr, cc = disk(center, int(np.min(center)*radius_coefs[polimer_type]), shape=section_shape)
+        mask = np.zeros(section_shape, dtype=int)
+        mask[rr, cc] = True
+
+        for i, img2d in enumerate(img3d):
+            img3d[i] = mask * img2d
+
+        for ax, i in zip(axes[1], [0, len(img3d) // 2, -1]):
+            ax.imshow(img3d[i], cmap="gray")
+
+        dm.save_plot(fig, "setup bin section", 'section '+ str(sample_id) + ' ' + sample_name)
+        save_to_h5(img3d, sample_name)
+
+    sample.close()
--- porosity_distribution_cubes.py
+++ porosity_distribution_cubes.py
@@ -1,29 +1,20 @@
 from porespy.generators import blobs
 import matplotlib.pyplot as plt
 import numpy as np
-import h5py
-from skimage.filters import threshold_otsu, threshold_multiotsu
-from scipy.ndimage import median_filter, gaussian_filter
-from skimage.filters.rank import mean as mean_filter
-from skimage.morphology import ball
-from skimage.draw import disk
-
-from icecream import ic
 
 import data_manager as dm
-from helper import crop
-from helper import crop, paste, get_2d_slice_of_sample_from_database
+from helper import crop, get_2d_slice_of_sample_from_database
 from phase_contrast_restoration import get_img as get_bin_img
-from phase_contrast_restoration import save as save_to_h5
+
 import file_paths
-import leviating_volume as lv
+
 
 plt.style.use('seaborn-whitegrid')
 plt.rcParams.update({'font.size':22})
 
 PIXEL_SIZE_MM = 0.8125 * 10**-3
 SAVE_IMG_SYNCHROTON_FOLDER = 'cubics synchrotron'
-SAVE_IMG_DESKTOP_SETUP_FOLDER = 'cubics setup'
+SAVE_IMG_DESKTOP_SETUP_FOLDER = 'setup bin section'
 
 
 def divide_image_into_cubic_fragments(img, edge_size):
@@ -108,7 +99,7 @@ def save_first_section_of_img(img_3d, file_id, edge_size):
     dm.save_plot(fig, SAVE_IMG_SYNCHROTON_FOLDER, f'section {file_id}')
 
 
-def calculate_synchrotron(edge_size = 400):
+def main_synchrotron(edge_size = 400):
     # ============= Обработка синхротронных данных =====================
     # для неоцентрированных образцов
     sample_crop_edges = {'123493': (None, None),
@@ -134,140 +125,9 @@ def calculate_synchrotron(edge_size = 400):
     get_porosity_histogram_disrtibution(img_fragments, file_id, bin_img.shape, PIXEL_SIZE_MM)
 
 
-def binarize_without_eppendorf(img3d, polimer_attenuation):
-    """
-    polimer_attenuation = "low", "high"
-    """
-    img3d_bin = []
-    for img2d in img3d:
-        thresholds = threshold_multiotsu(img2d)
-        if polimer_attenuation=="low":
-            #img2d_bin = np.logical_and(img2d < thresholds[1], img2d > thresholds[0])
-            # лучше вместе с эппендорфом
-            img2d_bin = img2d > thresholds[0]
-        elif polimer_attenuation=="high":
-            img2d_bin = img2d > thresholds[1]
-        
-        img3d_bin.append(img2d_bin)
-        
-    return np.asarray(img3d_bin)
-
-
-def plot_3_sections_multiotsu(img3d,
-                              polimer_attenuation,
-                              filename,
-                              radius_coef=0.9,
-                              folder=SAVE_IMG_DESKTOP_SETUP_FOLDER+"multy"):
-
-    fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(21, 21))
-    axes_plot, axes_hist, axes_bin = axes
-
-    section_indexes = [0, len(img3d)//2, -1]
-    img3d = gaussian_filter(img3d, sigma=3)
-
-    for ax_plot, ax_hist, i in zip(axes_plot, axes_hist, section_indexes):
-        ax_plot.imshow(img3d[i], cmap="gray")
-
-        ax_hist.hist(img3d[i].flatten(), bins=255, color="gray")
-        thresholds = threshold_multiotsu(img3d[i])
-        for thresh in thresholds:
-            ax_hist.axvline(thresh, color='red')
-
-    img3d = binarize_without_eppendorf(img3d, polimer_attenuation=polimer_attenuation)
-    img3d = lv.remove_levitating_stones(img3d)
-
-    for ax_bin, i in zip(axes_bin, section_indexes):
-        ax_bin.imshow(img3d[i], cmap="gray", interpolation=None)
-    
-        center = np.asarray(img3d[i].shape) // 2
-
-        rr, cc = disk(center, int(np.min(center)*radius_coef), shape=img3d[i].shape)
-        mask = np.zeros(img3d[i].shape, dtype=int)
-        mask[rr, cc] = True
-        mask = np.ma.masked_where(mask>0, mask)
-        ax_bin.imshow(mask, cmap="hsv", alpha=0.3)
-
-    dm.save_plot(fig, folder, 'section '+filename)
-
-
-polimer_attenuations_PDLG5002 = ["low",
-                                 "low",
-                                 "high",
-                                 "high",
-                                 "low",
-                                 "low", #manual 5
-                                 "high", #manual 6
-                                 "high", #manual 7
-                                 "high",
-                                 "high",
-                                 "low",
-                                 "high",
-                                 "high",
-                                 "high",
-                                 "high"]
-
-
-polimer_attenuations_PDL05 = ["low",
-                              "low",
-                              "low",
-                              "low",
-                              "low",
-                              "low", #manual 5
-                              "low", #manual 6
-                              "high", #manual 7
-                              "high", # 8
-                              "high", # 9
-                              "low", # 10
-                              "low", # 11
-                              "low", # 12
-                              "low", # 13
-                              "high"]
-
-
 if __name__=='__main__':
-    polimer_type = ["PDL-05", "PDLG-5002"][1]
-    radius_coefs = {"PDL-05": 0.9, "PDLG-5002": 0.95}
-    polimer_attenuation = {"PDL-05": polimer_attenuations_PDL05,
-                           "PDLG-5002": polimer_attenuations_PDLG5002}
-    
-    paths = file_paths.get_benchtop_setup_paths(polimer_type)
+    pass
 
-    for sample_id in range(len(paths)):
-        print(sample_id)
-        sample_name = list(paths.keys())[sample_id]
-        sample = h5py.File(paths[sample_name],'r')
-        
-        img3d = sample['Reconstruction'][:]
-        
-        fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(21, 14))
-        for ax, i in zip(axes[0], [0, len(img3d) // 2, -1]):
-            ax.imshow(img3d[i], cmap="gray")
-
-        # preview sections
-        # plot_3_sections_multiotsu(img3d,
-        #                           polimer_attenuations_PDL05[sample_id],
-        #                           str(sample_id) + ' ' + sample_name)
-                                  
-        img3d = gaussian_filter(img3d, sigma=3)
-        img3d = binarize_without_eppendorf(img3d, polimer_attenuation=polimer_attenuation[polimer_type][sample_id])
-        img3d = lv.remove_levitating_stones(img3d)
-        
-        section_shape = img3d[0].shape
-        center = np.asarray(section_shape) // 2
-        rr, cc = disk(center, int(np.min(center)*radius_coefs[polimer_type]), shape=section_shape)
-        mask = np.zeros(section_shape, dtype=int)
-        mask[rr, cc] = True
-
-        for i, img2d in enumerate(img3d):
-            img3d[i] = mask * img2d
-
-        for ax, i in zip(axes[1], [0, len(img3d) // 2, -1]):
-            ax.imshow(img3d[i], cmap="gray")
-
-        dm.save_plot(fig, "setup bin section", 'section '+ str(sample_id) + ' ' + sample_name)
-        save_to_h5(img3d, sample_name)
-
-    sample.close()
 
 
 
