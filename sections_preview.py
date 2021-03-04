8808da2e9a7fa4a86026bc97912e051d298570c1
ankhafizov <ankhafizov1998@yandex.ru>
2021-03-04 17:32:56 +0300

    added fast section preview of bin objects

--- sections_preview.py
+++ sections_preview.py
@@ -1,9 +1,11 @@
 import porespy
 import numpy as np
+import matplotlib.pyplot as plt
 
 from phase_contrast_restoration import get_img as get_bin_img
 from phase_contrast_restoration import save as save_to_h5
 import file_paths
+import data_manager as dm
 
 if __name__=='__main__':
     
@@ -15,4 +17,7 @@ if __name__=='__main__':
     for sample_id in range(len(paths)):
         sample_name = list(paths.keys())[sample_id]
         img3d = get_bin_img(sample_name)
-        save_to_h5(~img3d, sample_name)
+        
+        fig, ax = plt.subplots()
+        ax.imshow(img3d[0], cmap="gray")
+        dm.save_plot(fig, "previews", f'{sample_id} bin ' + sample_name)
