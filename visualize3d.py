268a84e2827378fc1f914cddc9fdd93642e4f963
ankhafizov <ankhafizov1998@yandex.ru>
2021-03-05 18:46:09 +0300

    added generator of 3d images

---
+++ visualize3d.py
@@ -0,0 +1,23 @@
+from porespy import visualization
+from phase_contrast_restoration import get_img as get_bin_img
+import file_paths
+import matplotlib.pyplot as plt
+import data_manager as dm
+
+from skimage.measure import marching_cubes
+
+polimer_type = ["PDL-05", "PDLG-5002"][1]
+sample_ids = {"PDL-05": [10, 12, 14],  "PDLG-5002": [0, 2, 4]}
+for sample_id in sample_ids[polimer_type]:
+
+    paths = file_paths.get_benchtop_setup_paths(polimer_type)
+    sample_name = list(paths.keys())[sample_id]
+
+    img3d = ~get_bin_img(sample_name)
+    img2d = visualization.show_3D(img3d)
+
+    fig, ax = plt.subplots(figsize=(10,10))
+    ax.imshow(img2d)
+    dm.save_plot(fig, "3d visualization", f'{sample_id} 3d ' + sample_name)
+
+    # visualization.show_mesh(marching_cubes())
